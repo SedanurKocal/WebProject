eb6b39f41f22fd3690b1f63542ec050bf8a04511
Esra KIZILELMA <kizilelmaesra@gmail.com>
2022-12-20 21:09:17 +0300

    frontend ekleme

--- MyWebProject/Controllers/YCategoryController.cs
+++ MyWebProject/Controllers/YCategoryController.cs
@@ -1,12 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LayerB.Concrate;
+using LayerDA.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MyWebProject.Controllers
 {
     public class YCategoryController : Controller
     {
+        YCategoryMannager cm = new YCategoryMannager(new EFYCategoryRepositroy());
+
         public IActionResult Index()
         {
-            return View();
+            var values = cm.GetList();
+            return View(values);
         }
     }
 }
