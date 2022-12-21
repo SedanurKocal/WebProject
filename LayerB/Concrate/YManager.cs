78fa8ca553c0956ebf20aca126f06e1a60f42e3f
Esra KIZILELMA <kizilelmaesra@gmail.com>
2022-12-21 22:19:46 +0300

    hatalar var

---
+++ LayerB/Concrate/YManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml.Schema;
+using LayerB.Abstract;
+using LayerDA.Abstract;
+using LayerE.Concrate;
+
+public class YManager :IBYemekService 
+{
+	IYBlogDal yBlogDal;
+
+	public YManager(IYBlogDal yBlogDal)
+	{
+		this.yBlogDal = yBlogDal;
+	}
+
+	public void YBlogAdd(YBlog yblog)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void YBlogDelete(YBlog yblog)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void YBlogUpdate(YBlog yblog)
+	{
+		throw new NotImplementedException();
+	}
+
+	public List<YBlog> GetList()
+	{
+		return yBlogDal.GetAllList();
+	}
+
+	public YBlog GetById(int id)
+	{
+		throw new NotImplementedException();
+	}
+	public List<YBlog> GetBlogyID(int id)
+	{
+		return yBlogDal.GetAllList(x => x.YBID == id);
+	}
+
+	public List<YBlog> GetBlogListWithCategory()
+	{
+		return yBlogDal.GetListWithCategory();
+	}
+}
